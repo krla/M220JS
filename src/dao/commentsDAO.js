0326081c6364c08d7139cde783466073aa1b4493
Carla Ros <krlarosm@gmail.com>
2020-06-10 19:51:01 +0100

    delete comment

--- src/dao/commentsDAO.js
+++ src/dao/commentsDAO.js
@@ -45,7 +45,7 @@ export default class CommentsDAO {
     try {
       // TODO Ticket: Create/Update Comments
       // Construct the comment document to be inserted into MongoDB.
-      const commentDoc = { someField: "someValue" }
+      const commentDoc = { "name": user.name, "email": user.email, "movie_id": ObjectId(movieId), "text": comment, "date": date }
 
       return await comments.insertOne(commentDoc)
     } catch (e) {
@@ -70,8 +70,8 @@ export default class CommentsDAO {
       // Use the commentId and userEmail to select the proper comment, then
       // update the "text" and "date" fields of the selected comment.
       const updateResponse = await comments.updateOne(
-        { someField: "someValue" },
-        { $set: { someOtherField: "someOtherValue" } },
+        { "_id": ObjectId(commentId), "email": userEmail },
+        { "$set": { "text": text, "date": date } },
       )
 
       return updateResponse
@@ -94,10 +94,9 @@ export default class CommentsDAO {
     try {
       // TODO Ticket: Delete Comments
       // Use the userEmail and commentId to delete the proper comment.
-      const deleteResponse = await comments.deleteOne({
+      const deleteResponse = await comments.deleteOne({ email: userEmail,
         _id: ObjectId(commentId),
       })
-
       return deleteResponse
     } catch (e) {
       console.error(`Unable to delete comment: ${e}`)
